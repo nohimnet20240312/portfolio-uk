dd61d21feb1b89f8d0e0dde27e6c9e4e7a8ab5c8
Nohim hasitha <nohimhasitha@Nohims-MacBook-Air.local>
2025-08-25 15:51:15 +0530

    v5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./secs/hero";
 import Highlight from "./comps/Highlight";
 import DetailsCard from "./comps/DetailsCard";
 import Heading from "./comps/Heading";
+import Section from "./secs/section";
 
 let immigrationData = {
   Name: "Nohim hasitha weedagama arachchi",
@@ -25,14 +26,28 @@ let studyData = {
   residency: "Telford court hertfordshire AL10 9UT",
 };
 let skillData = {
-  "Reliability & Punctuality": "Arriving on time, consistent attendance, and completing shifts/tasks dependably.",
-  "Communication (Verbal & Written)": "Clear, polite communication with colleagues, managers and customers; able to write simple messages or reports.",
-  "Teamwork": "Working cooperatively, supporting colleagues, and contributing to a positive team environment.",
-  "Time Management & Prioritisation": "Balancing study and work, prioritising tasks under busy conditions, and meeting deadlines.",
-  "Customer Service": "Understanding customer needs, polite interaction, complaint handling and delivering positive experiences.",
-  "Adaptability & Flexibility": "Willingness to take varied tasks/shifts and adjust quickly to new procedures or busy periods."
+  "Reliability & Punctuality":
+    "Arriving on time, consistent attendance, and completing shifts/tasks dependably.",
+  "Communication (Verbal & Written)":
+    "Clear, polite communication with colleagues, managers and customers; able to write simple messages or reports.",
+  Teamwork:
+    "Working cooperatively, supporting colleagues, and contributing to a positive team environment.",
+  "Time Management & Prioritisation":
+    "Balancing study and work, prioritising tasks under busy conditions, and meeting deadlines.",
+  "Customer Service":
+    "Understanding customer needs, polite interaction, complaint handling and delivering positive experiences.",
+  "Adaptability & Flexibility":
+    "Willingness to take varied tasks/shifts and adjust quickly to new procedures or busy periods.",
 };
-
+let contactData = {
+  Email: <a href="mailto:nohim.net@gmail.com">nohim.net@gmail.com</a>,
+  "Phone number": "+94 76 272 2301",
+  Web: (
+    <a href="https://nohimnet20240312.github.io/portfolio-uk/">
+      https://nohimnet20240312.github.io/portfolio-uk/
+    </a>
+  ),
+};
 
 let availability = true;
 function App() {
@@ -54,7 +69,7 @@ function App() {
         <div
           className="layer absolute top-0 left-0 w-full h-full z-1000"
           style={{
-            backgroundColor: "rgba(255, 147, 41, 0.08)",
+            backgroundColor: "rgba(200, 73, 5, 0.21)",
             pointerEvents: "none",
           }}
         ></div>
@@ -81,24 +96,31 @@ function App() {
             data={studyData}
           ></DetailsCard>
         </div>
-        <div className="skills flex flex-col items-center  p-5 w-full steel">
-          <div className="row-1">
-            <Heading className="text-3xl mb-4 text-white  font-thin">Skills</Heading>
-          </div>
-          <div className="flex flex-col row-2">
-            {Array.from(Object.entries(skillData)).map(([key, value]) => {
-              return(<>
-              <div className="punch skill-card my-3 text-white p-3">
-              <div className="row-1 text-2xl">{key}</div>
-              <div className="row-2 font-light">
-                {value}
-              </div>
-            </div>
-              </>)
-            })}
-            
-          </div>
-        </div>
+        <Section heading={"Skills"} background="steel">
+          {Array.from(Object.entries(skillData)).map(([key, value]) => {
+            return (
+              <>
+                <div className="punch skill-card my-3 text-white p-3">
+                  <div className="row-1 text-2xl">{key}</div>
+                  <div className="row-2 font-light">{value}</div>
+                </div>
+              </>
+            );
+          })}
+        </Section>
+
+        <Section heading="Contact me" background={"var(--deep-night)"}>
+          {Array.from(Object.entries(contactData)).map(([key, value]) => {
+            return (
+              <>
+                <div className="punch skill-card my-3 text-white p-3">
+                  <div className="row-1 text-2xl">{key}</div>
+                  <div className="row-2 font-light">{value}</div>
+                </div>
+              </>
+            );
+          })}
+        </Section>
         <div className="contact-me">hello</div>
       </div>
     </>
--- my-app/src/secs/section.jsx
+++ my-app/src/secs/section.jsx
@@ -1,3 +1,18 @@
-export default function Section() {
-  return <></>;
+import Heading from "../comps/Heading";
+export default function Section({ heading, background, children }) {
+  return (
+    <>
+      <div
+        className="skills flex flex-col items-center  p-5 w-full steel"
+        style={{ backgroundColor: background }}
+      >
+        <div className="row-1">
+          <Heading className="text-3xl mb-4 text-white  font-thin">
+            {heading}
+          </Heading>
+        </div>
+        <div className="flex flex-col row-2">{children}</div>
+      </div>
+    </>
+  );
 }
